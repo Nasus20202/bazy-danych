d6a6f6805f591eef2ac35c807d7198b114172333
Krzysztof Nasuta <krzysztof.nasuta@wp.pl>
2023-12-17 20:51:35 +0100

    feat: add client deletion

---
+++ Part1/delete-client.sql
@@ -0,0 +1,13 @@
+USE Stores;
+
+DECLARE @SaleId INT = 10;
+DECLARE @ClientId INT = (SELECT Client_ID FROM Sales WHERE Sale_ID = @SaleId);
+
+SELECT * FROM Clients WHERE Client_ID = @ClientId;
+SELECT * FROM Sales WHERE Sale_ID = @SaleId;
+
+DELETE FROM Clients WHERE Client_ID = @ClientId;
+SELECT 'Client with ID ' + CAST(@ClientId AS VARCHAR) + ' deleted' AS Message;
+
+SELECT * FROM Clients WHERE Client_ID = @ClientId;
+SELECT * FROM Sales WHERE Sale_ID = @SaleId;
